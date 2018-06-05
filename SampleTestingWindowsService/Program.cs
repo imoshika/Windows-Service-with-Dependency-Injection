40ec7b4e81cd4efc03c3c26a0d0df85a41d6fe66
Imoshika <imoshika@calcey.com>
2018-06-05 09:50:30 +0530

    Applying dependency Injection to a windows service

---
+++ SampleTestingWindowsService/Program.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestingWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            var builder = new Autofac.ContainerBuilder();
+            builder.RegisterType<Service1>().As<ServiceBase>();   
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var component = scope.Resolve<Service1>();
+            }
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ SampleTestingWindowsService/SampleLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestingWindowsService
+{
+    public static class SampleLibrary 
+    {
+
+        public static void WriteErrorLog(Exception e)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\SampleLogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + e.Source.ToString().Trim() + "," + e.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        public static void WriteErrorLog(string message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\SampleLogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + message);
+
+                /*string text = File.ReadAllText(@"C:\Users\imoshika\source\repos\SampleTestingWindowsService\SampleTestingWindowsService\bin\Debug\SampleLogFile.txt");
+                text = text.Replace("some job", "new job");
+                File.WriteAllText(@"C:\Users\imoshika\source\repos\SampleTestingWindowsService\SampleTestingWindowsService\bin\Debug\SampleLogFile.txt", text);*/
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
